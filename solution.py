b47fc87bdbac7d6f1fcaa5064866b28d9fd76d09
Alexander Nizov <nizoff.as@yandex.ru>
2023-04-13 23:57:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+from scipy.stats import permutation_test
 
 chat_id = 457863109 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    def statistic(x, y, axis):
+        return np.mean(y, axis=axis) - np.mean(x, axis=axis)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.02
-    tstat, pvalue = ztest(y, x, 0, alternative='smaller')
-    return pvalue < alpha # Ваш ответ, True или False
+    statisticfloat, pvaluefloat, null_distributionndarray = permutation_test((y, x), statistic, vectorized=True, n_resamples=np.inf, alternative='less')
+    return pvaluefloat < alpha # Ваш ответ, True или False
